80b2b03cca7414198e08d1ae1c719af58a4f20df
Leonardo Estevão <leonardo33329168@gmail.com>
2024-10-20 11:20:09 -0300

    Funções

--- Física/formulas_mecanica.py
+++ Física/formulas_mecanica.py
@@ -1,4 +1,10 @@
-def mu(distancia=0,velocidade=0):
-    result = distancia/velocidade
-    return result
-    
+def vel_esc_media(posicao_final,posicao_inicial,tempo_inicial,tempo_final):
+    vel_media = (posicao_final-posicao_inicial)/(tempo_final-tempo_inicial)
+    return vel_media
+def mu(posicao_inicial=0,velocidade=0,tempo=0):
+    posicao_final = posicao_inicial+(velocidade*tempo)
+    return posicao_final
+
+def acel_esc_media(vel_final,vel_inicial,tempo_inicial,tempo_final):
+    acel_media = (vel_final-vel_inicial)/(tempo_final-tempo_inicial)
+    return acel_media    
